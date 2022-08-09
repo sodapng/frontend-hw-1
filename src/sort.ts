47b0ce11648c6edb4caf31191bd72c0a7a869b76
sodapng <sodapng@ya.ru>
2022-08-09 16:06:18 +0400

    feat: sort

--- src/sort.ts
+++ src/sort.ts
@@ -1,3 +1,16 @@
-const sort = () => {};
+const isString = (str: any) => typeof str === 'string';
+
+const sort = (str: string) => {
+  if (!isString(str)) {
+    throw new Error('INVALID_ARGUMENT');
+  }
+
+  return str
+    .toLowerCase()
+    .split(' ')
+    .sort((a, b) => a.length - b.length)
+    .map((n) => n.split('').sort().join(''))
+    .join(' ');
+};
 
 export default sort;
