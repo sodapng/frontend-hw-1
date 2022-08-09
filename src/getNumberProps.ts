c1142a7b3554ac25903d719cad961a1911c57e1f
sodapng <sodapng@ya.ru>
2022-08-09 22:01:28 +0400

    feat: getNumberProps

--- src/getNumberProps.ts
+++ src/getNumberProps.ts
@@ -1,3 +1,28 @@
-const getNumberProps = () => {};
+import { isNumber } from './sum';
+
+const isObject = (obj: any) =>
+  Object.prototype.toString.call(obj) === '[object Object]';
+
+const getNumberProps = (obj: { [key: string]: any }): string[] => {
+  if (!isObject(obj)) {
+    throw new Error('INVALID_ARGUMENT');
+  }
+
+  return Object.keys(obj)
+    .reduce((acc: string[], key) => {
+      const value = obj[key];
+
+      if (isNumber(value)) {
+        acc.push(key);
+      }
+
+      if (isObject(value)) {
+        return acc.concat(getNumberProps(value));
+      }
+
+      return acc;
+    }, [])
+    .sort();
+};
 
 export default getNumberProps;
