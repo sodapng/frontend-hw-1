48fad37d45f2730730e74adc0b0c4a4447cc4c6c
sodapng <sodapng@ya.ru>
2022-08-09 19:04:55 +0400

    feat: patchArrays

--- src/patchArrays.ts
+++ src/patchArrays.ts
@@ -1,5 +1,30 @@
 // @ts-nocheck
 
-const patchArrays = (): void => {};
+import { isNumber } from './sum';
+
+const patchArrays = (): void => {
+  Object.assign(Array.prototype, {
+    count: function (this: []) {
+      return this.length;
+    },
+
+    insert: function (this: [], idx: number, element: any) {
+      if (!isNumber(idx)) {
+        throw new Error('INVALID_ARGUMENT');
+      }
+
+      this.splice(idx < 0 ? 0 : idx, 0, element);
+
+      return this;
+    },
+
+    remove: function (this: [], element: any) {
+      const idx = this.findIndex((value) => value === element);
+      this.splice(idx, 1);
+
+      return this;
+    },
+  });
+};
 
 export default patchArrays;
