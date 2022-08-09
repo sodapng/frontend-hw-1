09c772c1bda9b13ef122d036d1deaf6e7cc9446a
sodapng <sodapng@ya.ru>
2022-08-09 19:13:15 +0400

    feat: multiply

--- src/multiply.ts
+++ src/multiply.ts
@@ -1,3 +1,17 @@
-const multiply = () => {};
+import { isNumber } from './sum';
+
+const multiply = (a: number) => {
+  if (!isNumber(a)) {
+    throw new Error('INVALID_ARGUMENT');
+  }
+
+  return (b: number) => {
+    if (!isNumber(b)) {
+      throw new Error('INVALID_ARGUMENT');
+    }
+
+    return a * b;
+  };
+};
 
 export default multiply;
