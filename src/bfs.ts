815ce0963946017da3139ffbd21a9cdbd720a2ac
sodapng <sodapng@ya.ru>
2022-08-10 00:35:08 +0400

    feat: bfs

--- src/bfs.ts
+++ src/bfs.ts
@@ -1,3 +1,26 @@
-const bfs = () => {};
+import { isObject } from './getNumberProps';
+
+const bfs = (graph: { [key: string]: string[] }, start = 'A') => {
+  if (!isObject(graph)) {
+    throw new Error('INVALID_ARGUMENT');
+  }
+
+  const visited = [start];
+  const queue = [start];
+
+  while (queue.length) {
+    const currentNode = queue.shift();
+    const destinations = graph[<string>currentNode];
+
+    for (const destination of destinations) {
+      if (!visited.includes(destination)) {
+        visited.push(destination);
+        queue.push(destination);
+      }
+    }
+  }
+
+  return visited;
+};
 
 export default bfs;
