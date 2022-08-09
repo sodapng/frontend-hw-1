5c6884c505b8d136dc060164e48e126c62e06d5b
sodapng <sodapng@ya.ru>
2022-08-09 20:44:13 +0400

    feat: planEvent

--- src/planEvent.ts
+++ src/planEvent.ts
@@ -1,3 +1,14 @@
-const planEvent = () => {};
+import { isNumber } from './sum';
+
+const planEvent = <T extends (...args: any) => any>(
+  cb: T,
+  timeout: number
+): Promise<ReturnType<T>> => {
+  if (typeof cb !== 'function' || !isNumber(timeout)) {
+    throw new Error('INVALID_ARGUMENT');
+  }
+
+  return new Promise((resolve) => setTimeout(() => resolve(cb()), timeout));
+};
 
 export default planEvent;
